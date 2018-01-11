93c904becb64103ef13c9d048b225ee0c8fab4d4
Aibol Kussain <Aibol_Kussain@epam.com>
2018-01-11 10:47:49 +0300

    submit: Submit my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function splitAny(any) {
-    // your solution here
+    if (typeof any === 'string') {
+      var result = [];
+      var list = any.split(' ');
+      if (list.length === 1) {
+        return list[0].split('');
+      }
+      else {
+        return list.map(function(any) {
+          return any.split('');
+        });
+      }
+    }
+
+    if (typeof any === 'number') {
+      var result = [];
+      var negative = false;
+      if (any < 0) {
+        negative = true;
+        any = any * (-1);
+      }
+      while(any > 0) {
+        result.unshift(any%10);
+        any = Math.floor(any / 10);
+      }
+      if (negative) {
+        result.unshift('-');
+      }
+      return result;
+    }
+
+    if (Array.isArray(any)) {
+      return any.map(function(item) {
+        return splitAny(item);
+      });
+    }
+
+    if (typeof any === 'object') {
+      var result = [];
+      Object.keys(any).forEach(function(key) {
+        if (key && any.hasOwnProperty(key)) {
+          result.push([key, any[key]]);
+        }
+      });
+      return result;
+    }
+
+    if (typeof any === 'function') {
+      return splitAny(any());
+    }
+
 }
